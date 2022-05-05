15e9d823a24bd3c4a16a80f0f34110724629ca75
tonycastle <castleantony6@gmail.com>
2022-05-05 23:26:25 +0200

    added dynamic routes

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
-import { getSortedPostsData } from "../libs/posts";
+import { getSortedPostsData } from "../lib/posts";
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
